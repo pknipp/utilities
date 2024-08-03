20e75100556a134e935b29205a27a5046cf181fd
Peter Knipp <peteraknipp@gmail.com>
2024-08-03 17:36:57 -0400

    Change "digit" to "digits"

--- web/utilities/significanceFormatter.php
+++ web/utilities/significanceFormatter.php
@@ -1,10 +1,12 @@
 <?php
 
 function makeHtml($data) {
-    $numberString = $data["number"];
-    $digitsString = $data["digits"];
+    $numberString = $data['number'];
+    $digitsString = $data['digits'];
     $number = filter_var($data['number'], FILTER_VALIDATE_FLOAT);
-    $digits = filter_var($data['digit'], FILTER_VALIDATE_INT);
+    $digits = filter_var($data['digits'], FILTER_VALIDATE_INT);
+    echo $number;
+    echo $digits;
     // Following is only temporary.
     $outputString = $number + $digits;
     return ['formattedNumber' => $outputString];
