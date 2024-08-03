b5c651efbe8f045bb4758a8096f6adff4c229b25
Peter Knipp <peteraknipp@gmail.com>
2024-08-03 07:08:13 -0400

    Change name of fn to match other stuff.

--- web/significanceFormatter.php
+++ web/significanceFormatter.php
@@ -1,6 +1,6 @@
 <?php
 
-function sigFigFormat($numberString, $digitsString) {
+function significanceFormat($numberString, $digitsString) {
     $number = filter_var($numberString, FILTER_VALIDATE_FLOAT);
     $digits = filter_var($digitsString, FILTER_VALIDATE_INT);
     $outputString = $number + $digits;
