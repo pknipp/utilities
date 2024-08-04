8eafa0ee2deaf0bbd6aed75c5668d0ef0ad98be6
Peter Knipp <peteraknipp@gmail.com>
2024-08-04 17:12:44 -0400

    Start DRY-ing background/instructions page.

---
+++ web/utilities/significanceFormatter/makeUtility.php
@@ -0,0 +1,9 @@
+<?php
+
+function makeUtility($utility) {
+    return [
+        'name' => $utility,
+        'background' => 'Talk about sig figs, including link to Wikipedia.',
+        'instructions' => '1st item is number, 2nd is # of sig figs',
+    ];
+}
