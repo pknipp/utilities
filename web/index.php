0f44d65d7049494425d1a9c79fe9ba70f6b25072
Peter Knipp <peteraknipp@gmail.com>
2024-08-04 17:55:19 -0400

    Key into $utilities properly before looping over it.

--- web/index.php
+++ web/index.php
@@ -41,8 +41,7 @@ $app->get('/', function(Request $request, Response $response, LoggerInterface $l
   return $twig->render($response, 'utilityList.twig', makeUtilities());
 });
 
-print_r($utilities['utilities']);
-foreach ($utilities as $utility) {
+foreach ($utilities['utilities'] as $utility) {
   $app->get("/$utility", function(Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
     $logger->debug("logging output from $utility route");
     require('./utilities/' . $utility . '/makeUtility.php');
