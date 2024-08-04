2d4d47c6d4305268394ac70f34072158e774df4b
Peter Knipp <peteraknipp@gmail.com>
2024-08-04 19:56:30 -0400

    Do the link to instructions more carefully.

--- web/index.php
+++ web/index.php
@@ -54,7 +54,8 @@ foreach (makeUtilities()['utilities'] as $utility) {
   foreach ($options as $option) {
     $app->get("/{$utility}/json{$option}", function(Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
       $path = $_SERVER['REQUEST_URI'];
-      return $twig->render($response, 'error.twig', ['path' => $path]);
+      $utility = explode("/", $path)[1];
+      return $twig->render($response, 'error.twig', ['path' => $path, 'instructions' => "/$instructions"]);
     });
   };
 }
