e5c9b62f16f51f12b771a82c9d54fef8d79b9dd8
Peter Knipp <peteraknipp@gmail.com>
2024-08-04 08:17:09 -0400

    Add $

--- web/index.php
+++ web/index.php
@@ -40,7 +40,7 @@ $app->get('/', function(Request $request, Response $response, LoggerInterface $l
   $logger->debug('logging output.');
   require('./makeUtilities.php');
   $utilities = makeUtilities();
-  logger->debug(json_encode($utilities));
+  $logger->debug(json_encode($utilities));
   return $twig->render($response, 'utilityList.twig', $utilities);
 });
 
