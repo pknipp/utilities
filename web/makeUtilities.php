489ffed2100e484bc1c6ffc465d63ed065975974
Peter Knipp <peteraknipp@gmail.com>
2024-08-05 07:55:37 -0400

    Try to isolate makeUtility function definitions.

--- web/makeUtilities.php
+++ web/makeUtilities.php
@@ -7,7 +7,8 @@ function makeUtilities() {
     $utilities = array();
     foreach ($names as $name) {
         require("./utilities/$name/makeUtility.php");
-        $utilities[$name] = makeUtility($name);
+        $utilities[$name] = makeUtility;
+        $utilities[$name] = $utilities[$name]($name);
     }
     return ['utilities' => $utilities];
 }
