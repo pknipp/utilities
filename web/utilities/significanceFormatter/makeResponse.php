e0c9fcfbb013a7254e16cce19d34be7367284cae
Peter Knipp <peteraknipp@gmail.com>
2024-08-08 11:15:36 -0400

    Minor changes.

--- web/utilities/significanceFormatter/makeResponse.php
+++ web/utilities/significanceFormatter/makeResponse.php
@@ -4,8 +4,14 @@ function makeResponse($data) {
     $numberString = $data['number'];
     $digitsString = $data['digits'];
     $number = filter_var($data['number'], FILTER_VALIDATE_FLOAT);
+    $sign = '';
+    if ($number < 0) {
+        $sign = '-';
+        $number = abs($number);
+    }
     $digits = filter_var($data['digits'], FILTER_VALIDATE_INT);
-    // Following is only temporary.
-    $outputString = $number + $digits;
+    // following is temporary
+    $outputString = strval($number);
+    $outputString = $sign . $outputString;
     return ['formattedNumber' => $outputString];
 }
