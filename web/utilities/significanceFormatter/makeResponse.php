ee6a05c3c85062a813cc54b87cb7de5d1f82ed1e
Peter Knipp <peteraknipp@gmail.com>
2024-08-09 21:27:23 -0400

    Attempt to format w/significance.

--- web/utilities/significanceFormatter/makeResponse.php
+++ web/utilities/significanceFormatter/makeResponse.php
@@ -9,9 +9,22 @@ function makeResponse($data) {
         $sign = '-';
         $number = abs($number);
     }
+    $prefixesPositive = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
+    $prefixesNegative = ['', 'm', 'micro', 'n', 'p', 'f', 'a'];
+    $log10Number = log10($number);
+    $exponent = floor($log10Number);
+    $triple = floor($exponent / 3);
+    $prefix = '';
+    if ($triple > 0) {
+        $triple = min($triple, count($prefixesPositive) - 1);
+        $prefix = $prefixesPositive[$triple];
+    } elseif ($triple < 0) {
+        $triple = max($triple, 1 - count($prefixesNegative));
+        $prefix = $prefixesNegative[$triple];
+    }
+    $mantissa = $number / pow(10, 3 * $triple);
     $digits = filter_var($data['digits'], FILTER_VALIDATE_INT);
-    // following is temporary
-    $outputString = strval($number);
-    $outputString = $sign . $outputString;
+    $mantissa = round($mantissa, $digits - $exponent);
+    $outputString = $sign . strval($mantissa) . $prefix;
     return ['formattedNumber' => $outputString];
 }
