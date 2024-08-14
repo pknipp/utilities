fe40f34ab268f1055027df580504617d868295db
Peter Knipp <peteraknipp@gmail.com>
2024-08-14 08:07:14 -0400

    Change triple to triples.

--- web/utilities/significanceFormatter/makeResponse.php
+++ web/utilities/significanceFormatter/makeResponse.php
@@ -9,24 +9,31 @@ function makeResponse($data) {
         $sign = '-';
         $number = abs($number);
     }
+    // uncomment following two lines after completion of testing
     $prefixesPositive = ['', 'k']; //'k', 'M', 'G', 'T', 'P', 'E'];
     $prefixesNegative = ['', 'm']; //, 'micro', 'n', 'p', 'f', 'a'];
     $log10Number = log10($number);
-    $exponent = floor($log10Number);
-    $triple = floor($exponent / 3);
-    $prefix = '';
-    if ($triple > 0) {
-        $triple = min($triple, count($prefixesPositive) - 1);
-        $prefix = $prefixesPositive[$triple];
-    } elseif ($triple < 0) {
-        $triple = max($triple, 1 - count($prefixesNegative));
-        $prefix = $prefixesNegative[-$triple];
-    }
-    // $exponent = 3 * $triple;
-    $mantissa = $number / pow(10, 3 * $triple);
     $digits = filter_var($data['digits'], FILTER_VALIDATE_INT);
-    $fac = pow(10, $exponent - 3 * $triple);
-    $mantissa = round($mantissa / $fac, $digits - 1) * $fac;
+    $offset = $digits - 1;
+    $exponent = floor($log10Number);
+    $mantissa = $number / pow(10, $exponent);
+    $decimal_dust = pow(10, -8);
+    if (abs(round($mantissa * pow(10, $offset), 0) - pow(10, $offset + 1)) < $decimal_dust) {
+        $mantissa /= 10;
+        $exponent++;
+    }
+    $mantissa *= pow(10, $offset);
+    $triples = floor($exponent / 3);
+    $prefix = '';
+    if ($triples > 0) {
+        $triple = min($triples, count($prefixesPositive) - 1);
+        $prefix = $prefixesPositive[$triples];
+    } elseif ($triples < 0) {
+        $triple = max($triples, 1 - count($prefixesNegative));
+        $prefix = $prefixesNegative[-$triples];
+    }
+    $fac = pow(10, $exponent - 3 * $triples);
+    $mantissa = round($mantissa / $fac, $offset) * $fac;
     $outputString = $sign . strval($mantissa) . $prefix;
     return ['formattedNumber' => $outputString];
 }
