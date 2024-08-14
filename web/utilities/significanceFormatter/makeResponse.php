13cc8a74d218f819c47ce4f3d43038be4955a39f
Peter Knipp <peteraknipp@gmail.com>
2024-08-14 08:44:18 -0400

    Simplify edge case.

--- web/utilities/significanceFormatter/makeResponse.php
+++ web/utilities/significanceFormatter/makeResponse.php
@@ -14,15 +14,16 @@ function makeResponse($data) {
     $prefixesNegative = ['', 'm']; //, 'micro', 'n', 'p', 'f', 'a'];
     $log10Number = log10($number);
     $digits = filter_var($data['digits'], FILTER_VALIDATE_INT);
-    $offset = $digits - 1;
+    $precision = $digits - 1;
     $exponent = floor($log10Number);
     $mantissa = $number / pow(10, $exponent);
     $decimal_dust = pow(10, -8);
-    if (abs(round($mantissa * pow(10, $offset), 0) - pow(10, $offset + 1)) < $decimal_dust) {
+    // edge case: rounding causes mantissa to shift up to 10.
+    if (abs(round($mantissa, $precision) - 10) < $decimal_dust) {
         $mantissa /= 10;
         $exponent++;
     }
-    // $mantissa *= pow(10, $offset);
+    // $mantissa *= pow(10, $precision);
 
     $triples = floor($exponent / 3);
     $prefix = '';
@@ -34,7 +35,7 @@ function makeResponse($data) {
         $prefix = $prefixesNegative[-$triples];
     }
     $fac = pow(10, $exponent - 3 * $triples);
-    $mantissa = round($mantissa / $fac, $offset) * $fac;
+    $mantissa = round($mantissa / $fac, $precision) * $fac;
     $outputString = $sign . strval($mantissa) . $prefix;
     return ['formattedNumber' => $outputString];
 }
