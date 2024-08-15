49042d0499a3a1571865b5f9b48943aa2b375be0
Peter Knipp <peteraknipp@gmail.com>
2024-08-14 22:40:08 -0400

    Flesh out intro for significanceFormatter.

--- web/utilities/significanceFormatter/makeUtility.php
+++ web/utilities/significanceFormatter/makeUtility.php
@@ -4,8 +4,8 @@ function makeSignificanceFormatterUtility($name) {
     return [
         'name' => $name,
         'pretty' => 'Significance Formatter',
-        'description' => 'This formats a number with a specified number of significant digits (aka sigfigs).',
-        'background' => 'Talk about sig figs, including link to Wikipedia.',
-        'instructions' => '1st item is number, 2nd is # of sig figs',
+        'description' => 'This formats a number with a specified number of <a href="https://en.wikipedia.org/wiki/Significant_figures">significant digits</a> (aka sigfigs).',
+        'background' => 'Roughly speaking, the number of significant digits in a quantity equals its number of nonzero digits.  See <a href="https://en.wikipedia.org/wiki/Significant_figures">Wikipedia</a> for a more precise definition.',
+        'instructions' => "Immediately after <tt>{$name}</tt> in the url, type /<number>/<digits>, where <number> is the particular quantity, and <digits> is the number of significant digits with which you want this quantity expressed.",
     ];
 }
