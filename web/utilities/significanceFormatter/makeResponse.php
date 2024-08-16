2f371e5834555f0f28637c3127daa2e10479d9ac
Peter Knipp <peteraknipp@gmail.com>
2024-08-16 12:01:54 -0400

    Add some comments.

--- web/utilities/significanceFormatter/makeResponse.php
+++ web/utilities/significanceFormatter/makeResponse.php
@@ -5,7 +5,7 @@ function makeResponse($data) {
     $digitsString = $data['digits'];
     $numberValidated = filter_var($numberString, FILTER_VALIDATE_FLOAT);
     if (!$numberValidated) {
-        return ['error' => "{$numberString} param cannot be parsed as a number."];
+        return ['error' => "One param ({$numberString}) cannot be parsed as a number."];
     }
     $sign = '';
     if ($numberValidated < 0) {
@@ -14,13 +14,19 @@ function makeResponse($data) {
     }
     $prefixesPositive = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
     $prefixesNegative = ['', 'm', 'µ', 'n', 'p', 'f', 'a'];
+    //Invoking log10 is the easiest way to count digits to left of decimal point.
     $log10Number = log10($numberValidated);
-    $digits = filter_var($data['digits'], FILTER_VALIDATE_INT);
-    $precision = $digits - 1;
+    $digitsValidated = filter_var($digitsString, FILTER_VALIDATE_INT);
+    if (!$digitsValidated) {
+        return ['error' => "Number of significant digits ({$digitsString}) cannot be parsed as an integer."];
+    } else if ($digitsValidated < 1) {
+        return ['error' => "Number of significant digits ({$digitsValidated}) must be positive."];
+    }
+    $precision = $digitsValidated - 1;
     $exponent = floor($log10Number);
     $mantissa = $numberValidated / pow(10, $exponent);
     $decimal_dust = pow(10, -8);
-    // edge case: rounding causes mantissa to shift up to 10.
+    // edge case: (eventual) rounding causes mantissa to shift up to 10.
     if (abs(round($mantissa, $precision) - 10) < $decimal_dust) {
         $mantissa /= 10;
         $exponent++;
@@ -37,7 +43,7 @@ function makeResponse($data) {
     }
     $mantissa = strval(round($mantissa, $precision) * pow(10, $exponent - 3 * $triples));
     $hasDecimalPt = str_contains($mantissa, '.');
-    $zerosNeeded = $digits - (strlen($mantissa) - ($hasDecimalPt ? 1 : 0));
+    $zerosNeeded = $digitsValidated - (strlen($mantissa) - ($hasDecimalPt ? 1 : 0));
     if ($zerosNeeded > 0) {
         $mantissa .= (($hasDecimalPt ? '' : '.') . str_repeat('0', $zerosNeeded));
     }
