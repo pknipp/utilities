cda0de5170a2c2d2246f08fa1a036db2d40b1b35
Peter Knipp <peteraknipp@gmail.com>
2024-08-17 08:47:33 -0400

    Insert WIP.

--- web/utilities/axisMaker/makeUtility.php
+++ web/utilities/axisMaker/makeUtility.php
@@ -3,8 +3,8 @@
 function makeAxisMaker($name) {
     return [
         'name' => $name,
-        'pretty' => 'Axis Maker',
-        'description' => 'This renders an svg of an axis (either horizontal or vertical) for a graph.',
+        'pretty' => 'Axis Maker (WIP)',
+        'description' => 'This renders the svg for an axis (either horizontal or vertical) for a graph.',
         'background' => 'The main consideration here are that the tickmarks should be round numbers.',
         'instructions' => 'TBD',
         'examples' => 'TBD.  These should be svgs',
