5d4636e30dc4f7fcb4d7f1fccee58c58e3ebeede
Peter Knipp <peteraknipp@gmail.com>
2025-04-04 14:22:37 -0400

    Fix bug?

--- web/utilities/artHanger/makeUtility.php
+++ web/utilities/artHanger/makeUtility.php
@@ -3,8 +3,8 @@
 function makeArtHanger($name) {
     return [
         'name' => $name,
-        'pretty' => "pretty name",
-        'description' => "html string of description goes here",
+        'pretty' => "Art Hanger calculator",
+        'description' => "This enables the hanging of art when using a wire and screws fixed into two studs, when the center of the art does not necessarily coincide with either a stud or the midpoint between two studs",
         'background' => "html string of background goes here",
         'instructions' => "instructions string",
         'examples' => "self-explanatory",
