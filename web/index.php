d1d830f286e1619de6b901c3f142bfe6ea2c9146
Peter Knipp <peteraknipp@gmail.com>
2025-04-05 14:07:35 -0400

    Remove slash from end of route

--- web/index.php
+++ web/index.php
@@ -156,7 +156,7 @@ foreach ($options as $option) {
 
 foreach (['', '/json'] as $option1) {
   foreach ($options as $option2) {
-    $app->get("/artHanger{$option1}/{offset}/{stud}/{width}/{length}/{height}/$option2", function(string $offset, string $stud, string $width, string $length, string $height, Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
+    $app->get("/artHanger{$option1}/{offset}/{stud}/{width}/{length}/{height}$option2", function(string $offset, string $stud, string $width, string $length, string $height, Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
       $data = [
         'offset' => $offset,
         'stud' => $stud,
