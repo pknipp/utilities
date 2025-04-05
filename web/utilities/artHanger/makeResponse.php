def4de75737f5fe8b6be84cf4e88990f8e979d2d
Peter Knipp <peteraknipp@gmail.com>
2025-04-05 15:41:34 -0400

    Attempt again to write something to logs.

--- web/utilities/artHanger/makeResponse.php
+++ web/utilities/artHanger/makeResponse.php
@@ -106,7 +106,7 @@ function makeResponse($data) {
         if($data === null && json_last_error() !== JSON_ERROR_NONE){
             echo "json decode error: " . json_last_error_msg();
         }
-        error_log($data);
+        error_log(print_r($data, true));
         $y1 = sqrt($data['x']);
         $y2 = $xRight * $y1 / $xLeft;
         $widthPx = 1000;
